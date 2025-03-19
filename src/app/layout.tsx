eb4d8d60a873b7d98c6aebf40bffe014ad064097
Johannes Helleve <Johanshelleve@gmail.com>
2025-03-19 12:10:21 +0100

    this time?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,14 +23,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="min-h-screen flex flex-col">{children}</div>
+        {children}
       </body>
     </html>
   );
