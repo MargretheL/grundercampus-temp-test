73d299c8ed9538bfff92dd2d8f9da17166b61798
Johannes Helleve <Johanshelleve@gmail.com>
2025-03-19 11:59:46 +0100

    fixed logo

--- src/app/components/footercstm.tsx
+++ src/app/components/footercstm.tsx
@@ -4,17 +4,14 @@ export default function FooterCstm() {
         <div className="flex flex-col items-center relative">
             <p className="font-geist font-bold text-midnight-blue">FØLG OSS</p>
             <div className="flex items-center justify-center relative h-10">
-                <a href="https://www.google.com">
+                <a href="https://www.instagram.com/grundercampus/">
                     <img src="/images/instagram.svg" className="scale-60" alt="instagram" />
                 </a>
-                <a href="https://www.google.com">
-                    <img src="/images/facebook.svg" className="scale-60" alt="facebook" />
-                </a>
-                <a href="https://www.google.com">
+                <a href="https://www.linkedin.com/company/gr%C3%BCndercampus/">
                     <img src="/images/linkedin.svg" className="scale-60" alt="linkedin" />
                 </a>
             </div>
-            <a href="https://www.google.com">
+            <a href="mailto:kontakt@grundercampus.no">
                 <div className="flex flex-row justify-between gap-1.5">
                     <img src="/images/mail.svg" alt="logo" />
                     <p className="font-geist text-midnight-blue">kontakt@grundercampus.no</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ export default function Page() {
     <div className='bg-seashell'>
       <div className='bg-[url(/images/bg-small.png)] md:bg-[url(/images/bg-full.png)] bg-cover bg-center h-screen w-screen'>
         <div className="flex flex-col justify-between items-center pt-20 px-4 md:px-8  h-screen">
-          <div className='flex flex-col gap-30 xl:gap-20 items-center w-full '>
+          <div className='flex flex-col gap-30 md:gap-15 xl:gap-20 items-center w-full '>
             <div className='flex flex-row w-full justify-between '>
-              <div className='w-[15%] xl:w-[5%]'>  
+              <div className='w-[15%] md:w-[5%] lg:w-[5%]'>  
                 <img src='/images/logo-blue.png' alt='logo' />
               </div>
               <div className='flex flex-col items-end gap-2'>
@@ -23,7 +23,7 @@ export default function Page() {
               <HeaderCstm />
             </div>
           </div>
-            <div className='justify-end pb-12 xl:pb-0'>
+            <div className='justify-end pb-12 md:pb-0 xl:pb-0'>
               <FooterCstm />
             </div>    
           </div>
