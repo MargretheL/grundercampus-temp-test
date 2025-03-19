59393f09e137613119fb59f170eea5aa6be8de8c
Johannes Helleve <Johanshelleve@gmail.com>
2025-03-19 13:23:06 +0100

    test fra omfj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import FooterCstm from './components/footercstm';
 export default function Page() {
   return (
     <div className='bg-seashell'>
-      <div className='bg-[url(/images/bg-small.png)] md:bg-[url(/images/bg-full.png)] bg-cover bg-center h-screen w-screen'>
+      <div className='bg-[url(/images/bg-small.png)] md:bg-[url(/images/bg-full.png)] bg-cover bg-center h-[100svh] w-screen'>
         <div className="flex flex-col justify-between items-center pt-20 px-4 md:px-8  h-screen">
           <div className='flex flex-col gap-30 md:gap-15 xl:gap-20 items-center w-full '>
             <div className='flex flex-row w-full justify-between '>
