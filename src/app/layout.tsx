f04aa12410204f07bfd85f232fd469659f5e36a7
Johannes Helleve <Johanshelleve@gmail.com>
2025-03-20 18:06:23 +0100

    theme color fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
-        <meta name="theme-color" content="#1E40AF"/>
+        <meta name="theme-color" content="#FFF3EF"/>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
